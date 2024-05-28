7f0afe9907f9011f87741cb22a5c68aa38be5a31
AlexCapilla <acapilla93@gmail.com>
2024-05-27 23:51:54 -0700

    final commit, removed ds store via gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 const router = require("express").Router();
 
-const path = require("path");
-
 const apiRoutes = require("./apiRoutes");
+const htmlRoutes = require("./htmlRoutes");
 
 router.use("/api", apiRoutes);
-
-// HTML Routes
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+router.use("/", htmlRoutes);
 
 module.exports = router;
