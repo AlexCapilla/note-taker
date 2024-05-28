d665326e04613c86260eab5e7a44676674469474
AlexCapilla <acapilla93@gmail.com>
2024-05-27 22:27:47 -0700

    updated store.js and added routes to server.js

--- db/store.js
+++ db/store.js
@@ -11,18 +11,18 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFileAsync("db.json", "utf8")
+        return readFileAsync("db/db.json", "utf8")
     }
 
     write(note) {
-        return writeFileAsync("db.json", JSON.stringify(note))
+        return writeFileAsync("db/db.json", JSON.stringify(note))
     }
 
     getNotes() {
         return this.read().then((notes) => {
             let parsedNotes;
             try {
-                parsedNotes.push(JSON.parse(notes))
+                parsedNotes = [].concat(JSON.parse(notes))
             } catch(error) {
                 parsedNotes = []
             }
@@ -38,7 +38,7 @@ class Store {
             throw new Error("Title and Text cannot be blank")
         }
 
-        const newNote = { title, text, id: uuidv1()}
+        const newNote = { title, text, id: uuidv4()}
 
         return this.getNotes().then((notes) => [...notes, newNote])
         .then((updatedNotes) => this.write(updatedNotes))
--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+const store = require("./db/store");
 // Routes
 app.get("/api/notes", (req, res) => {
-    
+    store.getNotes().then((notes) => {
+        return res.status(200).json(notes)
+    }).catch((error) => res.status(500).json(error))
+})
+
+app.post("/api/notes", (req, res) => {
+    store.addNotes(req.body).then((note) => {
+        return res.status(200).json(note)
+    }).catch((error) => res.status(500).json(error))
 })
 
 
