89f83261193e16996759b4c93d6641e6363373d2
AlexCapilla <acapilla93@gmail.com>
2024-05-27 23:28:06 -0700

    optimized routes into route folder and made an index.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-
+const routes = require("./routes")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,30 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-const store = require("./db/store");
-// Routes
-app.get("/api/notes", (req, res) => {
-    store.getNotes().then((notes) => {
-        return res.status(200).json(notes)
-    }).catch((error) => res.status(500).json(error))
-})
-
-app.post("/api/notes", (req, res) => {
-    store.addNotes(req.body).then((note) => {
-        return res.status(200).json(note)
-    }).catch((error) => res.status(500).json(error))
-})
-
-app.delete("/api/notes/:id", (req, res) => {
-    store.removeNotes(req.params.id).then(() => {
-        return res.status(200).json({ delete: true, id: req.params.id })
-    }).catch((error) => res.status(500).json(error))
-})
-
-
-// Optimize
-
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
